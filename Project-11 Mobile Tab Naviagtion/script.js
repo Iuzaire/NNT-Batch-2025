3bddf20ac19fb9e43f37c9f8696afb593ecfa838
Uzair <uzairibrar55@gmail.com>
2025-03-14 10:55:36 +0500

    project 11 uploaded

---
+++ Project-11 Mobile Tab Naviagtion/script.js
@@ -0,0 +1,17 @@
+let img = document.getElementById('pic');
+
+function wrk() {
+  img.src = "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHdvcmt8ZW58MHx8MHx8fDA%3D";
+}
+
+function hme(){
+  img.src = "https://images.unsplash.com/photo-1741845518047-b1d9abe2c6b0?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D";
+}
+
+function blg(){
+  img.src = "https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D";
+}
+
+function abt(){
+  img.src = "https://images.unsplash.com/photo-1455849318743-b2233052fcff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YWJvdXQlMjB1c3xlbnwwfHwwfHx8MA%3D%3D";
+}
