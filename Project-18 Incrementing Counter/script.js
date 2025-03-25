a0224066100b3e86d0c2cf9140ad93600b9250d8
Uzair <uzairibrar55@gmail.com>
2025-03-25 11:04:39 +0500

    project 18- Code Improved

--- Project-18 Incrementing Counter/script.js
+++ Project-18 Incrementing Counter/script.js
@@ -1,48 +1,73 @@
-let twtNum = document.getElementById('Tfollower');
-
-let Ynum = document.getElementById('Yfollower');
-let insta = document.getElementById('Ifollower');
-
 let count1 = 0;
 let count2 = 0;
 let count3 = 0;
 
-setInterval(anim, 22)
-setInterval(anim2, 20)
-setInterval(anim3, 25)
+let print = setInterval(()=>{
 
-function anim() {
+  let twtNum = document.getElementById('Tfollower');
+  let Ynum = document.getElementById('Yfollower');
+  let insta = document.getElementById('Ifollower');
+  
+if(count1<950){
   count1++;
-
-  if (count1 > 1000) {
-    count1 = 1000;
-  } else {
-
-    twtNum.innerHTML = count1;
-  }
-
 }
+twtNum.innerHTML = count1; // twitter
 
-function anim2() {
+if(count2<1100){
   count2++;
-
-  if (count2 > 1200) {
-    count2 = 1230;
-  } else {
-
-    Ynum.innerHTML = count2;
-  }
-
 }
 
-function anim3() {
+Ynum.innerHTML = count2; // youtube
+
+if(count3<1240){
   count3++;
-
-  if (count3 > 1000) {
-    count3 = 1030;
-  } else {
-
-    insta.innerHTML = count3;
-  }
-
 }
+
+insta.innerHTML = count3; //instagram
+
+if(count3>=1240){
+  clearInterval(print);
+}
+},10)
+
+
+
+// setInterval(anim, 22)
+// setInterval(anim2, 20)
+// setInterval(anim3, 25)
+
+// function anim() {
+//   count1++;
+
+//   if (count1 > 1000) {
+//     count1 = 1020;
+//   } else {
+
+//     twtNum.innerHTML = count1;
+//   }
+
+// }
+
+// function anim2() {
+//   count2++;
+
+//   if (count2 > 1200) {
+//     count2 = 1230;
+//   } else {
+
+//     Ynum.innerHTML = count2;
+//   }
+
+// }
+
+// function anim3() {
+//   count3++;
+
+//   if (count3 > 1000) {
+//     count3 = 1030;
+//   } else {
+
+//     insta.innerHTML = count3;
+//   }
+
+// }
