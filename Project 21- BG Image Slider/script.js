7fdbc405738301da78c5d2d407dbd2366258971a
Uzair <uzairibrar55@gmail.com>
2025-04-07 11:30:42 +0500

    project image slier

---
+++ Project 21- BG Image Slider/script.js
@@ -0,0 +1,13 @@
+let leftBtn = document.getElementById('left');
+
+let rightBtn = document.getElementById('right');
+
+let images = document.querySelectorAll(".slide");
+
+console.log(images);
+
+rightBtn.addEventListener('click',()=>{
+
+
+  })
+
