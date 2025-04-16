4aa9e8644bc4c58d516afe1d65c85339e38683e8
Uzair <uzairibrar55@gmail.com>
2025-04-16 16:19:44 +0500

    project 24 Added

---
+++ Project 24- random-choice-picker/script.js
@@ -0,0 +1,79 @@
+
+
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+
+        randomSelect();
+    }
+});
+
+function createTags(input) {
+    const tags = input
+        .split(',')
+        .filter(tag => tag.trim() !== '')
+        .map(tag => tag.trim());
+
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
+    });
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        if (randomTag !== undefined) {
+            highlightTag(randomTag);
+
+            setTimeout(() => {
+                unHighlightTag(randomTag);
+            }, 100);
+        }
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+
+            // 🎉 Confetti Explosion
+            confetti({
+                particleCount: 150,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
+}
