8a97819ac3c398b486f01e243b2886017b2b994a
Uzair <uzairibrar55@gmail.com>
2025-04-25 14:46:43 +0500

    prj 37

--- Project 36- Bulb ON OFF/script.js
+++ Project 36- Bulb ON OFF/script.js
@@ -1,6 +1,10 @@
 const blb = document.getElementById("bulb");
 const batn = document.getElementById("btn");
+const circle = document.getElementById("crcle");
+
 
 batn.addEventListener("click", function(){
   blb.classList.toggle("active");
+  circle.classList.toggle("circle-active");
+  
 })
